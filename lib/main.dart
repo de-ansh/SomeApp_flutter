d4d29e3691802b90245916b99d70a0c969618da7
Ashish Mishra <anshm9448@gmail.com>
2022-03-28 19:18:55 +0530

    coomit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Some App",
     home: HomePage(),
-  ));
+    theme: ThemeData(
+      primarySwatch: Colors.teal,
+    ),
+  ),
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -16,9 +21,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Some App"),
       ),
-      body: Container(
-        child: Center(child:Text("hi Flutter")),
-      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(onPressed: ),
     );
   }
 }
