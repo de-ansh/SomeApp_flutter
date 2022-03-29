d70445ebf01feb7ed5a088da76fcb24c3567a3f4
Ashish Mishra <anshm9448@gmail.com>
2022-03-29 23:01:43 +0530

    commit5

--- lib/Pages/loginpage.dart
+++ lib/Pages/loginpage.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const 
-LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final userNameController = TextEditingController();
+  final passWordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login page"),),
+      appBar: AppBar(
+        title: Text("Login page"),
+      ),
       body: SingleChildScrollView(
         child: Column(
-          
+          children: [
+            Form(child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                    ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                    ),
+                ),
+              ],
+            ))
+          ],
         ),
-       ),
+      ),
     );
   }
 }
